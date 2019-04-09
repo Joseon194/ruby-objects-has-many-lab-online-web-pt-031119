2b4e7d8d87b977ecc9153520ca9ec56d5c07cf06
Dennis Chung <chungdennis6@gmail.com>
2019-04-09 02:18:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,29 +1,31 @@
 class Artist
-  attr_accessor :name, :songs
-  
-  def initialize(name)
+  attr_accessor :name
+  @@song_count = 0
+  def intialize(name)
     @name = name
-  end
-  
-  def songs
     @songs = []
-  end
-  
-  def add_song(song)
-     @@all << song
-     song.artist = self
-     @@song_count += 1
-   end
-   
-
-   def add_song_by_name(name, song)
-     song = Song.new(name, song)
-     @song << song
-     song.name = self
-     @@song_count += 1
-   end
-   
-   def self.song_count
-     @@song_count
-   end
 end
+
+def songs
+  @songs
+end
+
+def add_song(song)
+  @songs << song
+  song.artist = self
+  @@song_count += 1
+end
+
+def add_song_by_name(song_name)
+  song = Song.new(song_name)
+  @songs << song
+  song.artist = self
+  @@song_count += 1
+end
+
+def self.song_count
+  @@song_count
+end
+
+end
+end 
